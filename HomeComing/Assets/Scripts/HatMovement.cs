facb4d1cf7ac8c3dcd1b9a491433506e6002a121
Unknown <eitanwass@gmail.com>
2019-01-25 18:42:16 +0200

    A LOT

---
+++ HomeComing/Assets/Scripts/HatMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatMovement : MonoBehaviour
+{
+    private bool fly = false;
+    private Vector3 targetPos;
+    private Vector3 velocity = Vector3.zero;
+    private float time = 5f;
+
+    public void Fly(int direction)
+    {
+        direction = direction < 0 ? -1 : 1;
+
+        targetPos = new Vector3(transform.position.x + direction * 100, 0, transform.position.y);
+        fly = true;
+    }
+
+    private void Update()
+    {
+        if(fly)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, time);
+        }
+    }
+}
--- HomeComing/Assets/Scripts/PlayerController.cs
+++ HomeComing/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,77 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    public float jumpingForce = 3f;
+    public float jumpingForce = 7f;
+    public bool isGrounded = false;
+    private GameObject currentGround = null;
+    private float groundAngleThreshold = 0.5f;
 
     private Rigidbody2D rb;
+    private Animation anim;
+    private SpriteRenderer graphicsSprite;
+    public GameObject hat;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = this.GetComponentInChildren<Animation>();
+        graphicsSprite = this.GetComponentInChildren<SpriteRenderer>();
     }
 
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        bool jump = Input.GetKeyDown(KeyCode.W);
+        bool jump = Input.GetKeyDown(KeyCode.UpArrow);
+        bool attack = Input.GetKeyDown(KeyCode.Z);
 
         rb.velocity = new Vector2(horizontal * speed, 
             jump ? jumpingForce : rb.velocity.y);
+
+
+        if (horizontal > 0)
+            graphicsSprite.flipX = false;
+        if (horizontal < 0)
+            graphicsSprite.flipX = true;
+
+        if (isGrounded)
+        {
+            //if (jump)
+            //    anim.Play("HomieJump");
+            //if (rb.velocity.y < 0)
+            //    anim.Play("HomieGroundFall");
+            if (horizontal != 0)
+                anim.Play("HomieWalkRight");
+        }
+        if (attack)
+            Attack(horizontal);
+    }
+
+    private void Attack(float direction)
+    {
+        anim.Play();
+        hat.transform.SetParent(null);
+        hat.GetComponent<HatMovement>().Fly((int)direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (contact.normal.y > groundAngleThreshold)
+            {
+                currentGround = collision.gameObject;
+                isGrounded = true;
+                break;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject == currentGround)
+        {
+            currentGround = null;
+            isGrounded = false;
+        }
     }
 }
