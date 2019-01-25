03b5955039c7bc0200507306b467674672a61528
Unknown <eitanwass@gmail.com>
2019-01-25 15:47:02 +0200

    Initial Push

---
+++ HomeComing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpingForce;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+}
