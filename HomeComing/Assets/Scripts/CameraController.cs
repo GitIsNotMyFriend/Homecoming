7f23b5dc7527c0d71465ec76afae63fd0a1e7026
eitanwass <eitanwass@gmail.com>
2019-01-26 03:02:50 +0200

    Added so many things we can't even count

--- HomeComing/Assets/Scripts/CameraController.cs
+++ HomeComing/Assets/Scripts/CameraController.cs
@@ -7,8 +7,13 @@ public class CameraController : MonoBehaviour
     public Transform target;
     private Camera mainCamera;
 
-    public float dampTime = 0.15f;
+    private float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
+
+    public float cloudParallax = 30;
+
+    public Sprite backgroundImage;
+    public Transform cloudLayer;
 
     private void Start()
     {
@@ -22,8 +27,26 @@ public class CameraController : MonoBehaviour
             Vector3 point = mainCamera.WorldToViewportPoint(target.position);
             Vector3 delta = target.position - mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
             Vector3 destination = transform.position + delta;
+
+            Vector2 cameraProjection = GetCameraProjectionSize();
+            Vector2 backgroundSize = backgroundImage.bounds.max;
+
+            Vector2 cameraBound = backgroundSize - (cameraProjection / 2);
+
+            destination.x = Mathf.Clamp(destination.x, -cameraBound.x, cameraBound.x);
+            destination.y = Mathf.Clamp(destination.y, -cameraBound.y, cameraBound.y);
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+            cloudLayer.position = new Vector3((destination.x - Vector3.zero.x) / cloudParallax, 0, 0);
         }
 
+    }
+
+    private Vector2 GetCameraProjectionSize()
+    {
+        Vector2 topLeft = mainCamera.ScreenToWorldPoint(Vector3.zero);
+        Vector2 bottomRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+
+        return new Vector2(bottomRight.x - topLeft.x, bottomRight.y - topLeft.y);
     }
 }
--- HomeComing/Assets/Scripts/HatMovement.cs
+++ HomeComing/Assets/Scripts/HatMovement.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static PlayerController;
 
 public class HatMovement : MonoBehaviour
 {
     private bool fly = false;
     private Vector3 targetPos;
     private Vector3 velocity = Vector3.zero;
-    private float time = 5f;
 
-    public void Fly(int direction)
+    private const float animationTime = 2.5f;
+    private readonly float smoothStep = 2.5f;
+
+    private float time = 0.0f;
+
+    private int thisDirection;
+
+    private float startingScale;
+
+    public bool finishedAnimation = false;
+
+    public void Fly(Direction flyDirection)
     {
-        direction = direction < 0 ? -1 : 1;
+        startingScale = transform.localScale.x;
 
-        targetPos = new Vector3(transform.position.x + direction * 100, 0, transform.position.y);
+        thisDirection = flyDirection == Direction.RIGHT ? 1 : -1;
+
+        GetComponent<Animation>().Play();
+        targetPos = new Vector3(transform.position.x + thisDirection * 20, transform.position.y, 0);
         fly = true;
     }
 
@@ -21,7 +35,37 @@ public class HatMovement : MonoBehaviour
     {
         if(fly)
         {
-            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, time);
+
+            if (time < animationTime - 0.8f)
+            {
+                float boomerangEffect = (animationTime - 1.2f - Mathf.Max(animationTime - 1.2f, time)) * -thisDirection;
+                Vector3 boomerangVector = new Vector3(boomerangEffect * 300, 0, 0);
+                transform.position = Vector3.SmoothDamp(transform.position, targetPos - boomerangVector, ref velocity, smoothStep);
+
+                float endTransform = startingScale * (1.2f / Mathf.Pow(Mathf.Max(1.2f, time), 1.5f));
+
+                transform.localScale = new Vector3(endTransform, endTransform, 1f);
+            }
+
+            else if (time > animationTime)
+            {
+                fly = false;
+                // transform.localScale = new Vector3(startingScale, startingScale, 1f);
+                finishedAnimation = true;
+            }
+
+            else
+            {
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(fly)
+        {
+            time += Time.fixedDeltaTime;
         }
     }
 }
